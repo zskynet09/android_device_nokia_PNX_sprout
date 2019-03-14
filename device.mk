a5637bdb59b804734cae80f73ceb9c2684bab3a6
Jackeagle <jackeagle102@gmail.com>
2019-03-14 15:23:24 +0100

    Phoenix: Build netutils-wrapper-1.0
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- device.mk
+++ device.mk
@@ -120,6 +120,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
     $(LOCAL_PATH)/configs/media/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml
 
+# Net
+PRODUCT_PACKAGES += \
+    netutils-wrapper-1.0
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
