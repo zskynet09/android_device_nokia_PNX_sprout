0c70d1a3d25569c49c74d871f3a9f5c97f726288
Adam Shih <adamshih@google.com>
2020-03-11 14:43:45 +0800

    PNX: sepolicy: Update SELinux boot error
    Bug: 148033913
    Bug: 151195350
    Test: boot with no relevant avc error
    Change-Id: Ice7f8fb430eca6002a46b56284db8b8ead8303c7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -164,6 +164,7 @@ USE_SENSOR_MULTI_HAL := true
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Verified Boot
