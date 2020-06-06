5dbb382099b432aa953caaa32cd78d26a2fe2e71
DD3Boh <dade.garberi@gmail.com>
2020-06-06 16:59:43 +0530

    PNX: init: Override dalvik props according to total ram
    * Imported from https://github.com/log1cs/android_device_nokia_NB1/tree/evolution/init

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -126,6 +126,10 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_pnx
+TARGET_RECOVERY_DEVICE_MODULES := libinit_pnx
+
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/AllHWList/tp_double_tap"
 
