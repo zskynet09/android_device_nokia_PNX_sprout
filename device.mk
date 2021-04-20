7ea5a484a07bcc0e81845fd2b1c3264eac97dec2
Bruno Martins <bgcngm@gmail.com>
2021-04-20 21:56:48 +0100

    phoenix: Build power-off charging daemon dependency
    This must be explicitly built as of R. Fixes off mode charging.
    
    Change-Id: Ie013d25d55105b4eb2191b9fd625ed8e02c863c5

--- device.mk
+++ device.mk
@@ -125,6 +125,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.capabilityconfigstore@1.0
 
+# Charger
+PRODUCT_PACKAGES += \
+    libsuspend
+
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
