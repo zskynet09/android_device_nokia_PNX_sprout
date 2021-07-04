a05cc7711e7ce24284e90a413ee0b94296c77a84
zskynet09 <hakimfbacc@gmail.com>
2021-07-04 11:38:37 +0000

    Initial device tree * Based on dumps of PNX_00WW-RKQ1.200906.002-00WW_6_210

---
+++ lineage_Phoenix.mk
@@ -0,0 +1,21 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from Phoenix device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_DEVICE := Phoenix
+PRODUCT_NAME := lineage_Phoenix
+PRODUCT_BRAND := Nokia
+PRODUCT_MODEL := Nokia 8.1
+PRODUCT_MANUFACTURER := FIH
+
+PRODUCT_GMS_CLIENTID_BASE := android-nokia
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="PNX_00WW_FIH-user 11 00WW_6_210 00WW_6_210 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := Nokia/Phoenix_00WW/PNX_sprout:11/RKQ1.200906.002/00WW_6_210:user/release-keys
