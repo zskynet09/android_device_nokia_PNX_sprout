c090e520ed0b7c4ce165690d8651ceeafadefcdd
Ruthwar Ambeer <rambeer@codeaurora.org>
2021-10-09 14:28:30 +0530

    Phoenix: Add AtFwd hal to framework manifest.
    AtFwd hal is implemented as an android service and
    it stars from system partition, that is, it's not
    a traditional vendor hal and hence it should be
    declared in the framework manifest and not in the
    device manifest.
    This commit introduces new device specific framework
    manifest (framework_manifest.xml) and moves AtFwd hal
    from device to framework manifest.
    
    Change-Id: Iacdb5604a682b8462d16297f4b543928bb100282
    CRs-Fixed: 2287149

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,6 +121,7 @@ TARGET_ENABLE_MEDIADRM_64 := true
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml
 
 # Keystore
 TARGET_PROVIDES_KEYMASTER := true
