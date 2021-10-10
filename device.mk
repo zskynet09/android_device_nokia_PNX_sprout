8e0f451325481fa8284361e7bd92c1972cab3942
Luca Stefani <luca.stefani.ge1@gmail.com>
2021-10-10 08:52:21 +0530

    Phoenix: Import stock media profiles

--- device.mk
+++ device.mk
@@ -74,6 +74,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libavservices_minijail \
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/media_profiles_vendor.xml
+
 # Net
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
