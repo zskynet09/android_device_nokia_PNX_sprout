0356baecf03757b7f3f727ee02d9e58660feb30f
LuK1337 <priv.luk@gmail.com>
2021-10-11 09:13:53 +0530

    PNX: Build QTI bootctrl 1.1 HAL * Match stock PNX_00WW-RKQ1.200906.002-00WW_6_21F
    Change-Id: Ie5079fd4c40cb1f7414f2cba9f98d350f03047d8

--- device.mk
+++ device.mk
@@ -1,7 +1,7 @@
 # A/B
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl \
-    android.hardware.boot@1.1-impl.recovery \
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery \
     android.hardware.boot@1.1-service
 
 PRODUCT_PACKAGES += \
