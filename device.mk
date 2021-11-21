d7e0605373d9bfa5369db693a36217dff2fec6f0
zskynet09 <hakimfbacc@gmail.com>
2021-11-21 18:11:40 +0530

    Phoenix: Set up more A/B flags

--- device.mk
+++ device.mk
@@ -1,3 +1,13 @@
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+PRODUCT_PACKAGES += \
+    otapreopt_script
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
