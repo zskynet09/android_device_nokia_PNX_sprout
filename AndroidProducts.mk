7c04946cbddad9076ef93af456eff5e2d6e1a980
Dan Willemsen <dwillemsen@google.com>
2021-11-21 19:19:48 +0530

    Phoenix: Add common lunch choices Test: lunch  [see entries in list] Change-Id: Ia6a6f94696f2605dc2f3a932ea598f3cdeb3bed0

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_PNX_sprout.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_PNX_sprout-user \
+    lineage_PNX_sprout-userdebug \
+    lineage_PNX_sprout-eng
