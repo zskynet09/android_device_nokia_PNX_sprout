6eda4d5dbf7d01736a7bb234ce5f4fb774897968
LuK1337 <priv.luk@gmail.com>
2021-11-21 18:36:53 +0530

    PNX: Include QSSI sepolicy * Now that device/qcom/sepolicy is non-vendor only   we are able to do that.
    Change-Id: I4c83a8e30cfa11e45be2f06eb7f444de7f68d219
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,6 +84,9 @@ TARGET_USERIMAGES_USE_F2FS := true
 # Security patch level
 VENDOR_SECURITY_PATCH := 2021-08-01
 
+# Sepolicy
+include device/qcom/sepolicy/SEPolicy.mk
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
