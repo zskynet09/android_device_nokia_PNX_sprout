50f415c989ccbb11bb94c374e96e6c64139bb387
zskynet09 <hakimfbacc@gmail.com>
2021-11-21 20:32:33 +0530

    Phoenix: Import odm,product & system_ext props from stock * Pulled from PNX_00WW-RKQ1.200906.002-00WW_6_210

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,7 +81,10 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Properties
+TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
