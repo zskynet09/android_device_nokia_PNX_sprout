5ffa95817ed8901b5546aa2c44746224470acbbb
zskynet09 <hakimfbacc@gmail.com>
2021-11-21 18:40:37 +0530

    [TEMP]Phoenix: Set SELinux to permissive & disable forceencryption

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,6 +41,7 @@ BOARD_KERNEL_CMDLINE := \
     androidboot.usbcontroller=a600000.dwc3 \
     swiotlb=1 \
     loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_BOOT_HEADER_VERSION := 1
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
