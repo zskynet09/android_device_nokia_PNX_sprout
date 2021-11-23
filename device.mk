6f2643a9028c8f316c4ecb13e7c31b9d3d0412e2
Notorious <35992771+TTTT55@users.noreply.github.com>
2021-11-23 10:33:45 +0530

    Phoenix: build WiFi display packages

--- device.mk
+++ device.mk
@@ -448,5 +448,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libnl
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+
 # Inherit vendor
 $(call inherit-product, vendor/nokia/Phoenix/Phoenix-vendor.mk)
