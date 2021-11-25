2666d6e7f55447583ea80acb48df42b0450db443
Bharat Pawar <bpawar@codeaurora.org>
2021-11-25 13:50:34 +0530

    Phoenix: Enable LED based vibrator Enable LED based vibrator for sdm710 targets.
    Change-Id: I079eefaefff77fe19304acc8749773febf91a7fc

--- device.mk
+++ device.mk
@@ -420,6 +420,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service
+
 # WiFi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
