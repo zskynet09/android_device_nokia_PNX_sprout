1d03cd8f1945f2e5269cbed3ffffe59b4549d582
Notorious <35992771+TTTT55@users.noreply.github.com>
2021-11-25 14:13:58 +0530

    Phoenix: Enable AVB and build disabled vbmeta.img * Pack vbmeta into payload

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,7 +77,8 @@ AB_OTA_PARTITIONS += \
     boot \
     dtbo \
     system \
-    vendor
+    vendor \
+    vbmeta
 
 # Audio
 AUDIO_FEATURE_ENABLED_AAC_ADTS_OFFLOAD := true
@@ -149,6 +150,11 @@ USE_SENSOR_MULTI_HAL := true
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
+
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
 
 # WiFi
 BOARD_WLAN_DEVICE := qcwcn
