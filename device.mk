35cc0ed2dd208270eaaaf5666a04b92f579533a2
ix5 <ix5@users.noreply.github.com>
2021-11-25 13:58:28 +0530

    Phoenix: Add protobuf vendorcompat lib * Our blobs are compiled against libprotobuf-full-cpp.so, but starting in R, Android is using a versioned naming approach, e.g. libprotobuf-cpp-full-3.9.1.so.
    See https://r.android.com/1109518
    
    The entry in public.libraries.txt is needed to allow the
    linker to find and use the lib outside the VNDK.
    
    See https://source.android.com/devices/tech/config/namespaces_libraries
    
    [wight554: added libprotobuf-cpp-lite as well]
    
    GiaSen: some alphabetical order
    
    Signed-off-by: Volodymyr Zhdanov <wight554@gmail.com>
    Change-Id: I28222c89e1e07017867088492a726ee9d45d8e6f
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- device.mk
+++ device.mk
@@ -267,6 +267,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-vendorcompat \
+    libprotobuf-cpp-lite-vendorcompat
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
