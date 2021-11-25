2b7b53b462aad60a1f1cd6d7878bcfee64c715c4
AndrielFR <andrielkogama2@gmail.com>
2021-11-25 10:24:52 +0530

    Phoenix: Build vendor.qti.hardware.camera.device@1.0.vendor Signed-off-by: AndrielFR <andrielkogama2@gmail.com>

--- device.mk
+++ device.mk
@@ -85,7 +85,7 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0.vendor \
 
 # Display
 PRODUCT_PACKAGES += \
