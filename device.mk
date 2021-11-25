6351441a2b38d93198c4913b8dbe6fd677699bb0
Rahul Krishna <Rahulkrishna585@gmail.com>
2021-11-25 10:23:07 +0530

    Phoenix: Explictly build vendor.qti.hardware.camera.device@1.0 * 64-bit variant isn't being built by default
    Change-Id: I75854d887464de063d1bf031c065cc1cc6c6c9b8

--- device.mk
+++ device.mk
@@ -109,6 +109,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.6 \
+    vendor.qti.hardware.camera.device@1.0 \
 
 # Display
 PRODUCT_PACKAGES += \
