203f3a975fe273b8f5bd1b34f5c9435d8fabeb0b
SparXFusion <s2234nadar@gmail.com>
2021-11-27 13:35:28 +0530

    Phoenix: Build libbtconfigstore from source

--- device.mk
+++ device.mk
@@ -61,6 +61,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
     com.qualcomm.qti.bluetooth_audio@1.0 \
+    libbtconfigstore \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
