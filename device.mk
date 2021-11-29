0f03fa1e4036933f334b1158958e9fc97d974e2b
Bruno Martins <bgcngm@gmail.com>
2021-11-30 04:49:39 +0530

    Phoenix: Enable ANT+ support Change-Id: I910d7913f765dd4dee7904b972c99199db2a75d1

--- device.mk
+++ device.mk
@@ -23,6 +23,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService \
+    com.dsi.ant@1.0.vendor
+
 # Atrace
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
