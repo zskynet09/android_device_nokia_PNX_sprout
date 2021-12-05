dc21b099fc5430ebe610059ace16d2856cbd8a79
TH779 <tongsj2333@gmail.com>
2021-12-05 05:34:29 +0530

    Phoenix: Enable DT2W feature
    Change-Id: Ie036a52d2475785c79751d474a907e3befa63c9c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -126,6 +126,9 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml
 
+# Power
+TARGET_TAP_TO_WAKE_NODE := "/proc/AllHWList/tp_double_tap"
+
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
