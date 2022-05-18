d0e84a090d25fc7b5eed331dad73e1916cb9dec4
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-18 19:33:26 +0530

    Phoenix: Build android.hidl.base@1.0 Change-Id: I5457ebbadd95e4c43af4f4dd2e1a1413a241d865

--- device.mk
+++ device.mk
@@ -68,6 +68,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
     $(LOCAL_PATH)/configs/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0.vendor \
+
 # IDC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/idc/goodix_fp.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/goodix_fp.idc \
