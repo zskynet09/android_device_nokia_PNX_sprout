2b185f406177daf5e392fbc9a84a6ed8c96fd676
Demon Singur <demonsingur@gmail.com>
2022-05-24 05:07:52 +0530

    Phoenix: Import fingerprint HIDL Change-Id: I733eea70dc637fa61c2e3a8036100de7febd71a9

--- device.mk
+++ device.mk
@@ -118,6 +118,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:system/etc/excluded-input-devices.xml
 
+# Fingerprint sensor
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.pnx
+
 # GPS
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/gps/,$(TARGET_COPY_OUT_VENDOR)/etc)
