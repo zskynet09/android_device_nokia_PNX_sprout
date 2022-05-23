d2c61dcb6dec71f7acdc1a9f28e7c3166d5307e5
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-23 16:15:11 +0530

    Phoenix: Build lineage trust HIDL Change-Id: Icfecbd97df7adccc51406ae667fbc3cb10865a5a

--- device.mk
+++ device.mk
@@ -436,6 +436,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Trust
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # Update Engine
 PRODUCT_PACKAGES += \
     update_engine \
