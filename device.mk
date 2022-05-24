4055ec4e0e2b7f3906db1b2320c580edbe896c9f
maxwen <max.weninger@gmail.com>
2022-05-24 22:26:17 +0530

    Phoenix: overlay-lineage: Enable higher aspect ratio & had a notch

--- device.mk
+++ device.mk
@@ -264,7 +264,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Permissions
 PRODUCT_COPY_FILES += \
