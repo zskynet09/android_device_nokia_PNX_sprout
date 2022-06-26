c33c637fe7c79a5618aad41ef161b82eb8e081db
zskynet09 <hakimfbacc@gmail.com>
2022-06-27 02:49:26 +0530

    Revert "[TEMP]Phoenix: Set SELinux to permissive & disable forceencryption"
    This reverts commit a8f23cc593c5cbfeb44a15b7b87a20ac89e544d6.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,7 +32,6 @@ BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0 msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 BOARD_KERNEL_CMDLINE += ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 androidboot.configfs=true
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3 swiotlb=1 loop.max_part=7
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_BOOTIMG_HEADER_VERSION := 1
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
