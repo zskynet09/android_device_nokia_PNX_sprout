3cd9f168fa5bb0d6704ca1778fb0007773c87587
Alexander Koskovich <zvnexus@outlook.com>
2022-07-14 14:49:52 +0530

    Phoenix: Remove virtual framebuffer - It's largely for testing purposes and it consumes    a large amount of memory. If your hardware doesn't    have graphics then it can be useful, which isn't the    case for shipped devices.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,7 +32,6 @@ BOARD_KERNEL_CMDLINE := \
     earlycon=msm_geni_serial,0xA90000 \
     androidboot.hardware=qcom \
     androidboot.console=ttyMSM0 \
-    video=vfb:640x400,bpp=32,memsize=3072000 \
     msm_rtb.filter=0x237 \
     ehci-hcd.park=3 \
     lpm_levels.sleep_disabled=1 \
