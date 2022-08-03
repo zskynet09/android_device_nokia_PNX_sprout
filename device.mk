cf214fc2865c6b10843b33606fa5cac7d32b9e41
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-08-03 19:59:40 +0530

    Phoenix: Uprev bluetooth audio HIDL impl to 2.1 * We get free LC3 support, isn't it poggers?
    Change-Id: I0f1713ee542102374183ef8088bcad62d3859c2f

--- device.mk
+++ device.mk
@@ -72,7 +72,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     audio.bluetooth.default \
     com.qualcomm.qti.bluetooth_audio@1.0 \
     libbtconfigstore \
