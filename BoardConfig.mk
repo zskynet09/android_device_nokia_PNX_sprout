25ad03b24213de03f5ee593e9f089a6b17f0a015
Michael Bestas <mkbestas@gmail.com>
2022-08-13 22:26:25 +0530

    Phoenix: Switch to SYSTEM_EXT_{PUBLIC,PRIVATE}_SEPOLICY_DIRS Fixes: warning: BOARD_PLAT_PRIVATE_SEPOLICY_DIR has been deprecated.     Use SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS instead. warning: BOARD_PLAT_PUBLIC_SEPOLICY_DIR has been deprecated.     Use SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS instead. Change-Id: I752602079de8ff4c5370fe3ec861b8746838d878

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -166,7 +166,7 @@ VENDOR_SECURITY_PATCH := 2021-12-01
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Symlinks
