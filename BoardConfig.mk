ab2e03f17ea45b14229c02c6bb1128751e0ef1e7
Albert I <kras@raphielgang.org>
2022-08-13 22:24:00 +0530

    Phoenix: sepolicy: Initial dontaudit rules Signed-off-by: Albert I <kras@raphielgang.org> Change-Id: I7d784ad0464a4dbcf9c478ecaa8a2f1ad32196f2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -165,6 +165,7 @@ VENDOR_SECURITY_PATCH := 2021-12-01
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Symlinks
