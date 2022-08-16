b82def4abc5bb62e38b2ea661ff0ef7a267d557b
Arseniy Graur <argraur@gmail.com>
2022-08-16 16:08:49 +0530

    Phoenix: Use Pixel offline charger animation Signed-off-by: Arseniy Graur <argraur@gmail.com> Change-Id: Ie0d25e37912fffd337731d4c5eb9e9ccf09a2696

--- arrow_PNX_sprout.mk
+++ arrow_PNX_sprout.mk
@@ -6,6 +6,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/arrow/config/common.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_PIXEL_CHARGER := true
 
 # Inherit from PNX_sprout device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
