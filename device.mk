c3a31e167603eda0b030e4be9052d4c116143dce
Wei Wang <wvw@google.com>
2022-08-16 16:34:32 +0530

    Phoenix: Remove QTI perfd Bug: 69270928 Test: Build and boot device and test camera Change-Id: I87b283206f462fb5c8ec9cdd303ae2934fe9bfc4 Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -323,8 +323,7 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock \
-    vendor.qti.hardware.perf@2.2.vendor
+    android.hardware.power.stats@1.0-service.mock
 
 # Protobuf
 PRODUCT_PACKAGES += \
