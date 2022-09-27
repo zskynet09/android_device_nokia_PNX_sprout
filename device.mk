653ad813eb5f146b5d5036150c7f8e208d8ad5af
Artem Borisov <dedsa2002@gmail.com>
2022-09-27 07:59:44 +0530

    Phoenix: Disable triple buffering * Historically, triple buffering was used to workaround missed VSYNC   in GPU composition usecases on devices with high resolution panels.   As a downside, an additional frame buffer increased input latency.   Nowadays, most GPUs are fast enough to fit the VSYNC period, rendering   triple framebuffers useless.
    Change-Id: I27e4874c068449b844f1a07176a1452f2ef747d3

--- device.mk
+++ device.mk
@@ -413,7 +413,6 @@ PRODUCT_SOONG_NAMESPACES += \
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=4096
 
 # Telephony
