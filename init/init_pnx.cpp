6d48afb528172654d275058f1c752b7af2fcc072
Emanuel <emanuelghub@gmail.com>
2022-11-04 10:50:34 -0700

    PNX: libinit: Set Verified Boot State to green & disable OEM unlock
    This helps with passing safetynet

--- init/init_pnx.cpp
+++ init/init_pnx.cpp
@@ -98,6 +98,8 @@ void vendor_load_properties()
     property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
     property_override("dalvik.vm.heapminfree", heapminfree);
     property_override("dalvik.vm.heapmaxfree", heapmaxfree);
+    property_override("ro.boot.verifiedbootstate", "green");
+    property_override("ro.oem_unlock_supported", "0");
 
 #ifdef __ANDROID_RECOVERY__
     std::string buildtype = GetProperty("ro.build.type", "userdebug");
