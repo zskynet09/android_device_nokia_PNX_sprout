4ffb397a51695a3550eb73efc7686f6d1e11c7d0
Michael Bestas <mkbestas@lineageos.org>
2022-12-27 21:31:40 +0530

    PNX: sepolicy: Use SYSTEM_EXT_{PUBLIC,PRIVATE}_SEPOLICY_DIRS Fixes: warning: BOARD_PLAT_PRIVATE_SEPOLICY_DIR has been deprecated.     Use SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS instead. warning: BOARD_PLAT_PUBLIC_SEPOLICY_DIR has been deprecated.     Use SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS instead. warning: BOARD_PLAT_PUBLIC_SEPOLICY_DIR has been deprecated.     Use SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS instead.
    Change-Id: I752602079de8ff4c5370fe3ec861b8746838d878

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -165,7 +165,7 @@ USE_SENSOR_MULTI_HAL := true
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Verified Boot
